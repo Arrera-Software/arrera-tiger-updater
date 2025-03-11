6755e7b9314130e6d1357f6d0c9f939b5f062f79
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-03-11 21:06:01 +0100

    Mise en place de la partie update

--- CTigerUpdater.py
+++ CTigerUpdater.py
@@ -19,8 +19,9 @@ class CTigerUpdater:
         # Creation de la fenetre
         self.__rootWin = self.__arrTk.aTK(title="Installateur d'Arrera Store",width=500,height=400)
         # Frame de la fenetre
-        self.__fAcceuil = self.__arrTk.createFrame(self.__rootWin,width=450,height=350,bg="red")
-        self.__fInstall = self.__arrTk.createFrame(self.__rootWin,width=450,height=350,bg="blue")
+        self.__fAcceuil = self.__arrTk.createFrame(self.__rootWin,width=450,height=350)
+        self.__fInstall = self.__arrTk.createFrame(self.__rootWin,width=450,height=350)
+        self.__fMaj = self.__arrTk.createFrame(self.__rootWin,width=450,height=350)
         # Widget de la fenetre
         # fAcceuil
         labelIMGSoft = self.__arrTk.createLabel(self.__fAcceuil,text="",image=imageSoft)
@@ -30,12 +31,16 @@ class CTigerUpdater:
                                                ppolice="Arial",ptaille=15,pstyle="bold",
                                                command=self.__install)
         btnUpdate = self.__arrTk.createButton(self.__fAcceuil,text="Mettre à jour",
-                                              ppolice="Arial",ptaille=15,pstyle="bold")
+                                              ppolice="Arial",ptaille=15,pstyle="bold",
+                                              command=self.__maj)
 
         # fInstall
         self.__labelInstall = self.__arrTk.createLabel(self.__fInstall,text="Installation en cours",
                                                        ppolice="Arial",ptaille=30,pstyle="bold")
 
+        # fMaj
+        self.__labelMaj = self.__arrTk.createLabel(self.__fMaj,text="Mise à jour en cours",
+                                                    ppolice="Arial",ptaille=30,pstyle="bold")
 
         # Affichage des widget
         self.__arrTk.placeCenter(self.__fAcceuil)
@@ -44,6 +49,7 @@ class CTigerUpdater:
         self.__arrTk.placeBottomLeft(btnInstall)
         self.__arrTk.placeBottomRight(btnUpdate)
         self.__arrTk.placeCenter(self.__labelInstall)
+        self.__arrTk.placeCenter(self.__labelMaj)
 
     def start(self):
         # Vérification de la connexion internet
@@ -98,4 +104,57 @@ class CTigerUpdater:
             self.__rootWin.after(200, self.__checkInstall)
         else:
             mbox.showinfo("Information", "Installation terminée avec succès.")
+            self.__rootWin.destroy()
+
+    def __maj(self):
+        # Check si le logiciel est installer
+        mbox.showinfo("Information",
+                      "Selectionner le dossier contenant Arrera Store.")
+        directory = fd.askdirectory()
+        while directory == "":
+            mbox.showerror("Erreur", "Veuillez sélectionner un emplacement valide.")
+            directory = fd.askdirectory()
+
+        sortieFolder = self.__tiger.setEmplacementArreraSoft(directory)
+        if (sortieFolder == True):
+            mbox.showinfo("Information", "Emplacement selectionné avec succès.")
+        else:
+            mbox.showerror("Erreur", "Erreur lors de l'enregistrement de l'emplacement.")
+            return False
+
+        sortie = self.__tiger.getSoftInstall()
+        if (len(sortie) == 0):
+            mbox.showerror("Erreur", "Le logiciel n'est pas installé.")
+            return False
+
+        if ("arrera-tiger" not in sortie):
+            mbox.showerror("Erreur", "Le logiciel n'est pas installé.")
+            return False
+
+        # Check si une mise à jour est disponible
+        sortie = self.__tiger.checkUpdate()
+        if (len(sortie) == 0):
+            mbox.showinfo("Information", "Le logiciel est à jour.")
+            return False
+
+        if ("arrera-tiger" not in sortie):
+            mbox.showinfo("Information", "Le logiciel est à jour.")
+            return False
+
+        # Lancement de la mise à jour
+        self.__thread = th.Thread(target=self.__tiger.install, args=("arrera-tiger",))
+        self.__thread.start()
+        self.__fAcceuil.place_forget()
+        self.__arrTk.placeCenter(self.__fMaj)
+        self.__rootWin.after(200, self.__checkMaj)
+        return True
+
+    def __checkMaj(self):
+        if (self.__thread.is_alive()):
+            text = self.__labelMaj.cget("text")
+            newText = text + "."
+            self.__labelMaj.configure(text=newText)
+            self.__rootWin.after(200, self.__checkMaj)
+        else:
+            mbox.showinfo("Information", "Mise à jour terminée avec succès.")
             self.__rootWin.destroy()
